e6651c86ee3c7e0ea1420670b0f1270d445180c9
FungiDee <fchawora@gmail.com>
2025-08-23 13:05:54 +0200

    search functionality using the api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function refreshWeather(response) {
+  let tempElement = document.querySelector("#temp");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  tempElement.innerHTML = `${Math.round(temperature)}°C`;
+}
+
+function searchCity(city) {
+  let apiKey = "fb4ad7840ee0bb09a3cf56df8ot50fea";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("Venice");
